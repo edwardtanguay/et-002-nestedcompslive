31063eef413025a46e96013a0c2e098233c14b53
Edward Tanguay (windows) <edward@tanguay.info>
2022-11-02 11:51:56 +0100

    books works without typescript

---
+++ src/components/Book.tsx
@@ -0,0 +1,22 @@
+interface IProps {
+	book: any;
+}
+
+export const Book = (props: IProps) => {
+	const { book } = props;
+	return (
+		<div className="book">
+			<a href={book.bookUrl} target="_blank">
+				<img className="cover" src={book.imageUrl} />
+			</a>
+			<div className="info">
+				<div className="title">
+					<a href={book.bookUrl} target="_blank">
+						{book.title}
+					</a>
+				</div>
+				<div className="author">{book.author}</div>
+			</div>
+		</div>
+	);
+};
--- src/components/Books.tsx
+++ src/components/Books.tsx
@@ -1,3 +1,5 @@
+import { Book } from "./Book";
+
 interface IBook {
 	id: number,
 	title: string;
@@ -14,21 +16,9 @@ export const Books = (props:IProps) => {
 	const { books } = props;
 	return (
 		<div className="books">
-			{books.map((book:IBook, index:number) => {
+			{books.map((book:IBook) => {
 				return (
-					<div className="book" key={index+1}>
-						<a href={book.bookUrl} target="_blank">
-							<img className="cover" src={book.imageUrl} />
-						</a>
-						<div className="info">
-							<div className="title">
-								<a href={book.bookUrl} target="_blank">
-									{book.title}
-								</a>
-							</div>
-							<div className="author">{book.author}</div>
-						</div>
-					</div>
+					<Book book={book} key={book.id}/>
 				);
 			})}
 		</div>
