476ea5867b0eb2735cbaee5284aa448382f9077d
Edward Tanguay (windows) <edward@tanguay.info>
2022-11-02 11:20:18 +0100

    json file is the database

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,5 @@
 import './App.scss';
-
-const books = [
-	{
-		id: 1,
-		title: 'Building Microservices',
-		author: 'Sam Newman',
-		bookUrl: 'https://edwardtanguay.vercel.app/techBooks?id=1',
-		imageUrl:
-			'https://edwardtanguay.vercel.app/customImages/techBooks/buildingMicroservices.jpg'
-	},
-	{
-		id: 2,
-		title: 'Gatsby E-Commerce',
-		author: 'Alex Libby',
-		bookUrl: 'https://edwardtanguay.vercel.app/techBooks?id=2',
-		imageUrl:
-			'https://edwardtanguay.vercel.app/customImages/techBooks/gatsbyEcommerce.jpg'
-	},
-	{
-		id: 3,
-		title: 'Rust Web Programming',
-		author: 'Maxwell Flitton',
-		bookUrl: 'https://edwardtanguay.vercel.app/techBooks?id=3',
-		imageUrl:
-			'https://edwardtanguay.vercel.app/customImages/techBooks/rustWeb.jpg'
-	}
-];
+import books from './data/books.json';
 
 function App() {
 	return (
