8e636ee9f962df33a356896c89fcfd504b304a44
Edward Tanguay (windows) <edward@tanguay.info>
2022-11-02 11:07:54 +0100

    titles showing

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,46 @@
-import './App.scss'
+import './App.scss';
+
+const books = [
+	{
+		id: 1,
+		title: 'Building Microservices',
+		author: 'Sam Newman',
+		bookUrl: 'https://edwardtanguay.vercel.app/techBooks?id=1',
+		imageUrl:
+			'https://edwardtanguay.vercel.app/customImages/techBooks/buildingMicroservices.jpg'
+	},
+	{
+		id: 2,
+		title: 'Gatsby E-Commerce',
+		author: 'Alex Libby',
+		bookUrl: 'https://edwardtanguay.vercel.app/techBooks?id=2',
+		imageUrl:
+			'https://edwardtanguay.vercel.app/customImages/techBooks/gatsbyEcommerce.jpg'
+	},
+  {
+    id: 3,
+    title: 'Rust Web Programming',
+    author: 'Maxwell Flitton',
+    bookUrl: 'https://edwardtanguay.vercel.app/techBooks?id=3',
+    imageUrl:
+      'https://edwardtanguay.vercel.app/customImages/techBooks/rustWeb.jpg'
+  }
+];
 
 function App() {
-  return (
-    <div className="App">
-    <h1>Book Site</h1>
-    <p>testing</p>
-    </div>
-  )
+	return (
+		<div className="App">
+			<h1>Book Site</h1>
+      <h2>There are {books.length} books.</h2>
+      {books.map((book, index) => {
+        return (
+          <div className="book" key={index}>
+            <div className="title">{book.title}</div>
+          </div>
+        )
+      })}
+		</div>
+	);
 }
 
-export default App
+export default App;
